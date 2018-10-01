906c0989d277c6b98bb538d6aa2e5d3bf794cf55
Patrick Delong <pgd22@njit.edu>
2018-09-30 21:53:29 -0400

    done!

--- proj.py
+++ proj.py
@@ -4,11 +4,10 @@ Semester: Fall 2018
 Written by: Patrick Delong pgd22
 Instructor:  Marvin Nakayama, marvin@njit.edu
 """
-import sys
 import string
 
 range_s1 = ["www."]  # s1
-range_s2 = string.ascii_lowercase  # s2
+range_l = string.ascii_lowercase  # s2
 range_s3 = [".com", ".com.co", ".co"]  # s3
 
 
@@ -17,23 +16,52 @@ def ask_input(msg):
     ans = input(msg)  # Save the input answer
     if ans == "y":  # User wants to continue entering in websites
         url = input("Enter website: ")  # Save the input website
-        parse_url(url)  # Here we will call our parse function to check if its accepted or rejected
+        flow = parse_url(url)  # Here we will call our parse function to check if its accepted or rejected
+        if flow == [1, 2, 3]:
+            print("Accepted and the flow was ", flow)
+        elif flow == [2, 3]:
+            print("Accepted and the flow was ", flow)
+        else:
+            print("Rejected and the flow was ", flow)
         ask_input("Would you like to enter another website (y or n)?: ")  # Recursive call
     elif ans == "n":  # User is done
+        print("Bye!!")
         return  # End the program
     else:
         ask_input("Must type 'y' or 'n': ")  # User didn't format the answer right
 
 
-# Parse the url character by character
+# Parse the url character by character and return wether it was accepted or rejected
 def parse_url(url):
+    sub_str = ""  # Start with a blank sub string to build and compare
+    state = 0  # We have no state yet
+    s_flow = []
+
     for char in url:
-        print(char)
+        sub_str += char
+
+        if char is '.' and sub_str in range_s1 and state == 0:
+            s_flow += [1]
+            sub_str = ''
+            state = 2
+            continue
+
+        elif char is '.' and sub_str[0:-1].islower() and state != 3 and sub_str not in range_s3:
+            s_flow += [2]
+            sub_str = '.'
+            state = 3
+            continue
+
+    if state == 3 and sub_str in range_s3:
+        s_flow += [state]
+        return s_flow
+
 
 print("Project 1 for CS 341\n"
       "Semester: Fall 2018\n"
       "Written by: Patrick Delong\ pgd22\n"
       "Instructor:  Marvin Nakayama, marvin@njit.edu\n")
+
 
 # Get input from the user OR cat in the file from stdout to stdin using command cat cases.txt | python3 proj.py
 ask_input("Would you like to enter a website?(enter 'y' -> yes, 'n' -> no): ")
